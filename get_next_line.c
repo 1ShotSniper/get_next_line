edc8dbc041c26401c4d119132ff1f2b9d6952eb5
Dylan Slogrove <dslogrov@student.wethinkcode.co.za>
2018-05-28 18:28:31 +0200

    fixed memory issue

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 11:48:49 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/05/28 18:14:52 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/05/28 18:26:48 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-static t_list	*get_file_buffer(size_t fd, t_list *start)
+static char		*get_file_buffer(size_t fd, t_list *start)
 {
 	t_list	*tmp;
 
@@ -21,12 +21,12 @@ static t_list	*get_file_buffer(size_t fd, t_list *start)
 	while (tmp)
 	{
 		if (tmp->content_size == fd)
-			return (tmp);
+			return (tmp->content);
 		tmp = tmp->next;
 	}
 	tmp = ft_lstnew(NULL, fd);
 	ft_lstadd(&start, tmp);
-	return (tmp);
+	return (tmp->content);
 }
 
 static size_t	ft_endl_pos(const char *s)
@@ -45,34 +45,34 @@ int				get_next_line(const int fd, char **line)
 	size_t			bytes_read;
 	static t_list	*buffer_list;
 	char			*temp;
-	t_list			*f_buf;
+	char			*f_buf;
 
 	f_buf = get_file_buffer(fd, buffer_list);
 	if (fd < 0 || line == NULL || read(fd, local_buffer, 0) < 0)
 		return (-1);
-	if (!f_buf->content)
-		f_buf->content = ft_strnew(1);
+	if (!f_buf)
+		f_buf = ft_strnew(1);
 	while ((bytes_read = read(fd, local_buffer, BUFF_SIZE)))
 	{
 		local_buffer[bytes_read] = 0;
-		temp = ft_strjoin(f_buf->content, local_buffer);
-		free(f_buf->content);
-		f_buf->content = temp;
-		if (!f_buf->content)
-			return (-1);
-		if (ft_strchr(f_buf->content, '\n'))
+		temp = ft_strjoin(f_buf, local_buffer);
+		free(f_buf);
+		f_buf = temp;
+		//if (!f_buf)
+		//	return (-1);
+		if (ft_strchr(f_buf, '\n'))
 			break ;
 	}
-	if (bytes_read < BUFF_SIZE && !f_buf->content)
+	if (bytes_read < BUFF_SIZE && !f_buf)
 		return (0);
-	*line = ft_strndup(f_buf->content, ft_endl_pos(f_buf->content));
-	if (ft_strlen(*line) <= ft_strlen(f_buf->content))
+	*line = ft_strndup(f_buf, ft_endl_pos(f_buf));
+	if (ft_strlen(*line) <= ft_strlen(f_buf))
 	{
-		temp = ft_strdup(f_buf->content + ft_strlen(*line) + 1);
-		free(f_buf->content);
-		f_buf->content = temp;
+		temp = ft_strdup(f_buf + ft_strlen(*line) + 1);
+		free(f_buf);
+		f_buf = temp;
 	}
 	else
-		ft_strdel((char **)&f_buf->content);
+		ft_strdel((char **)&f_buf);
 	return (1);
 }
