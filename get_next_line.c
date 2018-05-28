89c5948438f22ebcec4a5c8545244c1c34a28303
Dylan Slogrove <dslogrov@student.wethinkcode.co.za>
2018-05-28 17:02:18 +0200

    First commit

---
+++ get_next_line.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/28 11:48:49 by dslogrov          #+#    #+#             */
+/*   Updated: 2018/05/28 16:59:21 by dslogrov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+size_t	ft_endl_pos(const char *s)
+{
+	char	*c;
+
+	c = (char *)s;
+	while (*c && *c != '\n')
+		c++;
+	return (c - s);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	char		buffer[BUFF_SIZE + 1];
+	size_t		bytes_read;
+	static char	*read_string;
+	char		*temp;
+
+	if (fd < 0 || line == NULL || read(fd, buffer, 0) < 0)
+		return (-1);
+	if (!read_string)
+		read_string = ft_strnew(1);
+	while ((bytes_read = read(fd, buffer, BUFF_SIZE)))
+	{
+		buffer[bytes_read] = 0;
+		temp = ft_strjoin(read_string, buffer);
+		free(read_string);
+		read_string = temp;
+		if (!read_string)
+			return (-1);
+		if (ft_strchr(read_string, '\n'))
+			break ;
+	}
+	if (bytes_read < BUFF_SIZE && !*read_string)
+		//unsure if this should set line
+		return (0);
+	*line = ft_strndup(read_string, ft_endl_pos(read_string));
+	if (ft_strlen(*line) <= ft_strlen(read_string)) //check this condition
+	{
+		temp = ft_strdup(read_string + ft_strlen(*line) + 1);
+		free(read_string);
+		read_string = temp;
+	}
+	else
+		ft_strdel(&read_string);
+	return (1);
+}
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/24 16:01:00 by dslogrov          #+#    #+#             */
+/*   Updated: 2018/05/28 12:21:11 by dslogrov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
