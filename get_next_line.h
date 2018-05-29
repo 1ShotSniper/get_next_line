fe50b446b8e007a8b1d7247654e534897a1fcce3
Dylan Slogrove <dslogrov@student.wethinkcode.co.za>
2018-05-29 15:34:48 +0200

    Changed buff size

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/24 16:01:00 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/05/29 11:20:12 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/05/29 15:34:30 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 511
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft/libft.h"
