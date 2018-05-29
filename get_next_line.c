83cbb223edfd896df8018ded28dc2a293b4b50c8
Dylan Slogrove <dslogrov@student.wethinkcode.co.za>
2018-05-29 11:58:31 +0200

    Got GNL working, just need to shorten

--- get_next_line.c
+++ get_next_line.c
@@ -6,27 +6,27 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 11:48:49 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/05/28 18:26:48 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/05/29 11:57:00 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-static char		*get_file_buffer(size_t fd, t_list *start)
+static t_list	*get_file_buffer(size_t fd, t_list **start)
 {
 	t_list	*tmp;
 
-	tmp = start;
+	tmp = *start;
 	while (tmp)
 	{
 		if (tmp->content_size == fd)
-			return (tmp->content);
+			return (tmp);
 		tmp = tmp->next;
 	}
-	tmp = ft_lstnew(NULL, fd);
-	ft_lstadd(&start, tmp);
-	return (tmp->content);
+	tmp = ft_lstnew("\0", fd);
+	ft_lstadd(start, tmp);
+	return (tmp);
 }
 
 static size_t	ft_endl_pos(const char *s)
@@ -45,34 +45,34 @@ int				get_next_line(const int fd, char **line)
 	size_t			bytes_read;
 	static t_list	*buffer_list;
 	char			*temp;
-	char			*f_buf;
+	t_list			*f_buf;
 
-	f_buf = get_file_buffer(fd, buffer_list);
 	if (fd < 0 || line == NULL || read(fd, local_buffer, 0) < 0)
 		return (-1);
-	if (!f_buf)
-		f_buf = ft_strnew(1);
+	f_buf = get_file_buffer(fd, &buffer_list);
+	if (!f_buf->content)
+		f_buf->content = ft_strnew(1);
 	while ((bytes_read = read(fd, local_buffer, BUFF_SIZE)))
 	{
 		local_buffer[bytes_read] = 0;
-		temp = ft_strjoin(f_buf, local_buffer);
-		free(f_buf);
-		f_buf = temp;
-		//if (!f_buf)
-		//	return (-1);
-		if (ft_strchr(f_buf, '\n'))
+		temp = ft_strjoin(f_buf->content, local_buffer);
+		free(f_buf->content);
+		f_buf->content = temp;
+		if (!f_buf->content)
+			return (-1);
+		if (ft_strchr(f_buf->content, '\n'))
 			break ;
 	}
-	if (bytes_read < BUFF_SIZE && !f_buf)
+	if (bytes_read < BUFF_SIZE && !*(char *)(f_buf->content))
 		return (0);
-	*line = ft_strndup(f_buf, ft_endl_pos(f_buf));
-	if (ft_strlen(*line) <= ft_strlen(f_buf))
+	*line = ft_strndup(f_buf->content, ft_endl_pos(f_buf->content));
+	if (ft_strlen(*line) < ft_strlen(f_buf->content))
 	{
-		temp = ft_strdup(f_buf + ft_strlen(*line) + 1);
-		free(f_buf);
-		f_buf = temp;
+		temp = ft_strdup(f_buf->content + ft_strlen(*line) + 1);
+		free(f_buf->content);
+		f_buf->content = temp;
 	}
 	else
-		ft_strdel((char **)&f_buf);
+		ft_strdel((char **)&f_buf->content);
 	return (1);
 }
