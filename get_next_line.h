c697f2be77bb7af96fa20e4a98639884e1442fb9
Dylan Slogrove <dslogrove@gmail.com>
2018-07-31 10:54:45 +0200

    Fixed libft include

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/24 16:01:00 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/06/13 13:03:06 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/07/31 10:53:26 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 # define BUFF_SIZE 11000
 # include <unistd.h>
 # include <stdlib.h>
-# include "libft/libft.h"
+# include <libft.h>
 
 int		get_next_line(const int fd, char **line);
 
